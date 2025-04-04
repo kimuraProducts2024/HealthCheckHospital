f07e575571e2549eb84a6474fd1e6f413543562b
kimuraProducts2024 <kimura_0222taku@outlook.jp>
2025-04-05 00:06:04 +0900

    first commit add login

---
+++ HealthCheckHospital/src/main/java/com/healthcheck/hospital/HealthCheckHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.healthcheck.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCheckHospitalApplication.class, args);
+	}
+
+}
